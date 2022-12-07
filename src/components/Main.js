36f9394ef679fc8ed4f9bddc2e3a5f26b118616e
Oleg672 <oleg6080826@mail.ru>
2022-12-07 19:18:13 +0300

    4 commit

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+
+function Main(props) {
+    return (
+        <div className='main'>
+            < Sidebar/>
+        </div>
+    );
+}
+
+export default Main;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import sidebar__Img from "../images/sidebar.png"
+import sidebar__foto from "../images/foto.png"
+<sidebar></sidebar>
+
+function Sidebar(props) {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__top">
+                <img src={sidebar__Img} alt="" className="sidebar__topImg" />
+                <div className="sidebar__foto-wrapper">
+                    <img src={sidebar__foto} alt="" className="sidebar__foto" />
+                </div>
+            </div>
+            <div className="sidebar__inner">
+                <h1 className="sidebar__title">Дмитрий Валак</h1>
+                <h2 className="sidebar__subtitle">блог front-end разработчика</h2>
+                <div className="sidebar__social">
+
+                    <a href="#" className="sidebar__social-inst"></a>
+                    <a href="#" className="sidebar__social-vk"></a>
+                    <a href="#" className="sidebar__social-pinterest"></a>
+
+                </div>
+                <p className="sidebar__description">
+                    Front-end разработчик. Практик верстки сайтов.
+                    Созданием сайтов занимаюсь с 2012 года. Работал в нескольких ИТ компаниях и наработал более 10 000 часов в создании сайтов различной сложности.
+                </p>
+                <div className="sidebar__buttons">
+                    <button className="sidebar__btn clr-red">Мои работы</button>
+                    <button className="sidebar__btn">Написать мне</button>
+                </div>
+            </div>
+
+
+
+        </div>
+    );
+}
+
+export default Sidebar;
