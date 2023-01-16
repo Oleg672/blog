04912dd8f1f2ecd3b1f1a418243d384950d7eea0
Oleg672 <oleg6080826@mail.ru>
2023-01-16 20:08:35 +0300

    Updates

---
+++ precache-manifest.697a4c72d0eef0e9c1b9710be605c46a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "9dd2a4f11f2cb13e5b12",
+    "url": "/blog/static/css/main.6a041fd4.chunk.css"
+  },
+  {
+    "revision": "9dd2a4f11f2cb13e5b12",
+    "url": "/blog/static/js/main.9dd2a4f1.chunk.js"
+  },
+  {
+    "revision": "561126de61029355b316",
+    "url": "/blog/static/js/1.561126de.chunk.js"
+  },
+  {
+    "revision": "319195b179fdae54ce22",
+    "url": "/blog/static/js/2.319195b1.chunk.js"
+  },
+  {
+    "revision": "c3ca3e219931c2642292",
+    "url": "/blog/static/js/runtime~main.c3ca3e21.js"
+  },
+  {
+    "revision": "7fa7e722fa9df5bf30da08e6c3405785",
+    "url": "/blog/static/media/send.7fa7e722.svg"
+  },
+  {
+    "revision": "4453dad68d1d03818c52fddc2dab9181",
+    "url": "/blog/static/media/sendImg.4453dad6.svg"
+  },
+  {
+    "revision": "88b50c504a19dfb309593cf84085365d",
+    "url": "/blog/static/media/story1.88b50c50.png"
+  },
+  {
+    "revision": "c281a1ce2f9c3d987e52ef9653a412f7",
+    "url": "/blog/static/media/story2.c281a1ce.png"
+  },
+  {
+    "revision": "ab49f02ad78de0659974bb9bb6df3ec8",
+    "url": "/blog/static/media/story3.ab49f02a.png"
+  },
+  {
+    "revision": "88507ebbff94fb23022f2ee3a32a29a5",
+    "url": "/blog/static/media/story4.88507ebb.png"
+  },
+  {
+    "revision": "ce7c50a2671eca51e704b87688f3b578",
+    "url": "/blog/static/media/sidebar.ce7c50a2.png"
+  },
+  {
+    "revision": "d77027d800fd422f85c48cc7c46a4ca7",
+    "url": "/blog/static/media/foto.d77027d8.png"
+  },
+  {
+    "revision": "a48cfc45f46f7de03f189e26f199b97c",
+    "url": "/blog/static/media/works1.a48cfc45.png"
+  },
+  {
+    "revision": "93d48352ca206fec1f33fdf78d52607e",
+    "url": "/blog/static/media/works2.93d48352.png"
+  },
+  {
+    "revision": "d529b18d71fd671389e52ed5c2328620",
+    "url": "/blog/static/media/inst.d529b18d.svg"
+  },
+  {
+    "revision": "d70b6361b009e36f7d45b3fa1dd6592a",
+    "url": "/blog/static/media/pint.d70b6361.svg"
+  },
+  {
+    "revision": "63be1e2b3e9df594fcc4f5adfabfa801",
+    "url": "/blog/static/media/vk.63be1e2b.svg"
+  },
+  {
+    "revision": "39054c3530d8e366ed74bafa2f16a493",
+    "url": "/blog/static/media/videoPlay.39054c35.svg"
+  },
+  {
+    "revision": "c025fd535ca10dfea26dc10e27017d6d",
+    "url": "/blog/static/media/rect.c025fd53.svg"
+  },
+  {
+    "revision": "761a78c093638638e7d3198840cc5930",
+    "url": "/blog/index.html"
+  }
+];
