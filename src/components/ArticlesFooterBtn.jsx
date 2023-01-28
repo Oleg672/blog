bb20e97c71f886add8cb263aabe3437b71031f46
Oleg672 <oleg6080826@mail.ru>
2023-01-28 20:16:04 +0300

    modal

--- src/components/ArticlesFooterBtn.jsx
+++ src/components/ArticlesFooterBtn.jsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 
 
 function ArticlesFooterBtn({ PagePagination1, ArticlePagination1, countPage }) {
-
+    
     return (
         <div className='NavBtn'>
-            <button className="NavBtn__btn" onClick={(value) => { (PagePagination1 > 1) ? (ArticlePagination1(parseInt(PagePagination1) -parseInt(1))): (ArticlePagination1(PagePagination1)) }}>&lt;</button>
-            <button value={1} className="NavBtn__btn" onClick={(value) => { ArticlePagination1(parseInt(value.target.value)); }} >1</button>
-            <button value={2} className="NavBtn__btn" onClick={(value) => { ArticlePagination1(parseInt(value.target.value)); }} >2</button>
-            <button value={3} className="NavBtn__btn" onClick={(value) => { ArticlePagination1(parseInt(value.target.value)); }} >3</button>
-            <button className="NavBtn__btn" onClick={(value) => { (PagePagination1 < countPage) ? (ArticlePagination1(parseInt(PagePagination1) +parseInt(1))) : (ArticlePagination1(PagePagination1)) }}>&gt;</button>
+            <button className="NavBtn__btn" onClick={(e) => { (PagePagination1 > 1) ? (ArticlePagination1(parseInt(PagePagination1) - parseInt(1))) : (ArticlePagination1(PagePagination1)) }} disabled={PagePagination1 > 1 ? false : true} >&lt;</button>
+            <button type='checkbox' className="NavBtn__btn active" onClick={(e) => { ArticlePagination1(parseInt(e.target.value)); }} value={1}>1</button>
+            <button type='checkbox' className="NavBtn__btn" onClick={(e) => { ArticlePagination1(parseInt(e.target.value)); }} value={2}>2</button>
+            <button type='checkbox' className="NavBtn__btn" onClick={(e) => { ArticlePagination1(parseInt(e.target.value)); }} value={3}>3</button>
+            <button className="NavBtn__btn" onClick={(e) => { (PagePagination1 < countPage) ? (ArticlePagination1(parseInt(PagePagination1) + parseInt(1))) : (ArticlePagination1(PagePagination1)) }} disabled={PagePagination1 < countPage ? false : true} >&gt;</button>
         </div>
     );
 }
